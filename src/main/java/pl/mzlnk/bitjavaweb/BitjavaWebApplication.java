4ffb31a13879b1b132c12005bc59b5cc3097aad1
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2020-05-22 15:50:24 +0200

    init - added sources

---
+++ src/main/java/pl/mzlnk/bitjavaweb/BitjavaWebApplication.java
@@ -0,0 +1,13 @@
+package pl.mzlnk.bitjavaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitjavaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BitjavaWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mzlnk/bitjavaweb/BitjavaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mzlnk.bitjavaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitjavaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
